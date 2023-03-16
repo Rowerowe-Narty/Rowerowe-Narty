e862e5f1a00da6c23e3e80e5e29198ba24bab459
Konrad <108412658+KondyKox@users.noreply.github.com>
2023-03-16 16:34:37 +0100

    Add files via upload

---
+++ main_menu/main.js
@@ -0,0 +1,25 @@
+var buttons = $('.btn');
+var logo = $('img');
+
+//Funkcja po kliknięciu na przycisk
+function handleClick() {
+    buttons.animate({
+        opacity: 0,
+        transform: 'scale(0.5) translateY(-100px)'
+    }, {
+        duration: 500, // czas trwania animacji
+        easing: 'easeInBack', //funkcja ułatwiająca określenie przebiegu animacji
+        queue: false // ustawiamy, aby animacja nie była dodawana do kolejki animacji
+    });
+
+    logo.animate({
+        transform: 'translateX(-1000px)'
+    }, {
+        duration: 2000,
+        easing: 'easiInOut',
+        queue: false
+    });
+}
+
+//Dodajemy nasłuchiwanie kliknięcia na każdym przycisku
+buttons.click(handleClick);
